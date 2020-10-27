43212008a769f597e9f7d1174df589e4e59e41d7
Alumno <alumno@correo.com>
2020-10-27 15:04:32 -0700

    Entregado

---
+++ PracticaAutoLayOuts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticaAutoLayOuts
+//
+//  Created by Alumno on 10/27/20.
+//  Copyright © 2020 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
